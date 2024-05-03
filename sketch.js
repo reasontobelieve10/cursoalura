013e3230486607352e5ffb9e41df1b4183ea9890
reasontobelieve10 <167317327+reasontobelieve10@users.noreply.github.com>
2024-05-03 09:24:48 -0300

    Create sketch.js alura nome 03.05.2024
    Reunião 03.05.2024

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  text("Starlight",120, 200);
+  textSize(50)
+  textStyle
+  fill(color(255,102,204));
+}
